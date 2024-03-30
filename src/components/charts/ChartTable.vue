f07bda1a064d2cc8ecef0e1c6f0695a0f92f8c42
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:39 +0000

    ChartTable component created to show simple data tables

---
+++ src/components/charts/ChartTable.vue
@@ -0,0 +1,51 @@
+<template>
+    <DataTable :value="tableData">
+        <Column field="label" header="GROUP BY" />
+        <Column field="count" header="RESPONDENTS" />
+        <Column field="percent" header="% VALUE" />
+    </DataTable>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+// Props
+const props = defineProps({
+    jsonData: {
+        type: Object,
+    },
+    groupBy: {
+        type: Array
+    }
+})
+
+// Component States
+const totalCount = ref(0)
+const totalPercent = ref(0)
+
+// Computed properties 
+
+const tableData = computed(() => {
+    const data = props.jsonData
+    // Loop through overall jsondata and add count and percent
+    for (const item of data) {
+        totalCount.value += item.count;
+        totalPercent.value += item.percent;
+    }
+
+    // Create a total object
+    const totalObject = {
+        label: 'Total',
+        count: totalCount.value,
+        percent: totalPercent.value?.toFixed(0) + ' %'
+    }
+
+    // Now return it along the array
+    return [
+        ...data,
+        totalObject
+    ]
+
+})
+
+</script>
