52704ef9051eb450f4880e97a3411a38b1968121
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:40 +0000

    Colors changed

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,26 +1,32 @@
 // export const Color = Object.freeze({
-//   Red: '#FF0000',
-//   Blue: '#0000FF',
-//   Green: '#008000',
-//   Yellow: '#FFFF00',
-//   Orange: '#FFA500',
-//   Purple: '#800080',
-//   Pink: '#FFC0CB',
-//   Black: '#000000',
-//   White: '#FFFFFF',
-//   Gray: '#808080'
+// Red: '#FF6666', // Lighter version of Red
+// Blue: '#6666FF', // Lighter version of Blue
+// Green: '#66CC66', // Lighter version of Green
+// Yellow: '#FFFF99', // Lighter version of Yellow
+// Orange: '#FFC266', // Lighter version of Orange
+// Purple: '#CC66CC', // Lighter version of Purple
+// Pink: '#FFCCCC', // Lighter version of Pink
+// Black: '#666666', // Lighter version of Black
+// White: '#CCCCCC', // Lighter version of White
+// Gray: '#BFBFBF' // Lighter version of Gray
 // })
+
 export const Color = Object.freeze({
-  Red: '#FF6666', // Lighter version of Red
-  Blue: '#6666FF', // Lighter version of Blue
-  Green: '#66CC66', // Lighter version of Green
-  Yellow: '#FFFF99', // Lighter version of Yellow
-  Orange: '#FFC266', // Lighter version of Orange
-  Purple: '#CC66CC', // Lighter version of Purple
-  Pink: '#FFCCCC', // Lighter version of Pink
-  Black: '#666666', // Lighter version of Black
-  White: '#CCCCCC', // Lighter version of White
-  Gray: '#BFBFBF' // Lighter version of Gray
+  RoyalBlue: '#4169E1',
+  EmeraldGreen: '#32CD32',
+  RubyRed: '#FF4500',
+  GoldenYellow: '#FFD700',
+  AmethystPurple: '#9966CC',
+  Teal: '#008080',
+  Coral: '#FF7F50',
+  SlateGray: ' #708090',
+  Magenta: '#FF00FF',
+  Turquoise: '#40E0D0',
+  Peach: '#FFDAB9',
+  Lavender: '#E6E6FA',
+  OliveGreen: '#808000',
+  Crimson: '#DC143C',
+  IndigoBlue: '#4B0082'
 })
 
 const colorValues = Object.values(Color)
