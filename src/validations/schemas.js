43a55062d8e734bb27c07625c80425c1e07f4210
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:18 +0000

    Validation Schema added for Question Form

--- src/validations/schemas.js
+++ src/validations/schemas.js
@@ -2,13 +2,37 @@ import * as yup from 'yup'
 import { formErrorStore } from '@/stores'
 import { snakeToCamel } from '@/utils/text-formatter'
 import { showToast } from '@/utils/show-toast'
-
+import { QuestionType } from '@/constants'
+import AppError from '@/utils/app-error'
 // ===============> Schemas here
 
 // ============> Login Schema
 export const LoginSchema = yup.object({
   email: yup.string().email('Invalid Email').required('Email field is required'),
   password: yup.string().required('Password field cannot be empty')
+})
+
+// ============> Question Form Validation Schema
+export const QuestionSchema = yup.object().shape({
+  title: yup.object({
+    english: yup.string().required('Question Title is required'),
+    nepali: yup.string().required('Nepali Question Title is required')
+  }),
+  description: yup.object({
+    english: yup.string().required('Description is required'),
+    nepali: yup.string().required('Description in Nepali required')
+  }),
+  type: yup.string().required('Question type is mandatory'),
+  options: yup.array().when('type', {
+    is: (value) => value === QuestionType.RADIO || value === QuestionType.CHECKBOX,
+    then: () =>
+      yup.array().of(
+        yup.object({
+          optionName: yup.string().required('Option name is mandatory'),
+          optionValue: yup.string().required('Option value is mandatory')
+        })
+      )
+  })
 })
 
 // ================> Error validation handler
@@ -19,12 +43,13 @@ export const handleValidation = async (formData, schema) => {
     formErrorStore.clearErrors()
     return true
   } catch (error) {
+    console.log(error)
     if (error.inner && typeof error.inner === 'object') {
       const errors = errorHandler(error)
       formErrorStore.addErrors(errors)
       showToast('info', 'Invalid Data', 'Please input valid data')
     } else {
-      throw new error(error)
+      throw new AppError(error.status, error?.data?.message)
     }
   }
 }
