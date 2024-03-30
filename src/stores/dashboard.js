4108ac263ba39cca15d4139f7ca183fcfd7ea36b
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:34 +0000

    Dashboard store created with utilization of dashboard details fetch services

---
+++ src/stores/dashboard.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+import AppResponse from '@/utils/app-response'
+import DashboardService from '@/services/dashboard-service'
+
+export const useDashboardStore = defineStore('dashboard', () => {
+  // States
+  const dashboards = ref([])
+
+  // Getters
+
+  // Actions
+  // Get Dashboard Details
+  const getDashboardDetails = async (dashboardId) => {
+    const response = await DashboardService.getDashboardDetails(dashboardId)
+
+    if (response.statusCode === 200) {
+      return response.data
+    }
+    return null
+  }
+
+  // Helpers
+
+  // Returns
+  return {
+    // States
+    dashboards
+
+    // Getters
+
+    // Actions
+
+    // Helpers
+  }
+})
