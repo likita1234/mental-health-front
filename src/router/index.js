e4b4f9edd18a50c3e60e0cf5d780ad62a3b45da7
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:26 +0000

    Router correction for assessment form details

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const router = createRouter({
               }
             },
             {
-              path: '/:id',
+              path: ':id',
               name: 'form-details',
               props: true,
               component: () => import('../components/assessment-form/CustomAssessment.vue'),
