7501b4bd9910ac38045fbd1381589255b45fe173
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:34 +0000

    ChartType typo fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 import { AssessmentFormType } from './assessment-form-type'
-import { ChartType } from './chart-type'
+import { ChartType } from '@/constants/chart-type'
 import { ErrorStates } from '@/constants/error-states'
 import { LanguageType } from './language-type'
 import { QuestionType } from '@/constants/question-type'
 import { UserRole } from '@/constants/user-role'
 
-export { AssessmentFormType, ChartTypes, ErrorStates, LanguageType, QuestionType, UserRole }
+export { AssessmentFormType, ChartType, ErrorStates, LanguageType, QuestionType, UserRole }
