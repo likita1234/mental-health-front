3281ce304e1063f8a0c2afc5a8d8a0af4d0d5a01
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:36 +0000

    MetricData component configured to display metric data on the basis of metricId props

---
+++ src/components/metric/MetricData.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        {{ metricData }}
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+import { metricStore } from '@/stores';
+
+// Props
+const props = defineProps({
+    metricId: {
+        type: String,
+        required: true
+    }
+})
+
+// Component states
+const metricData = ref(null)
+
+onMounted(() => {
+    loadMetricData()
+})
+
+// Actions
+
+const loadMetricData = async () => {
+    metricData.value = await metricStore.getMetricData(props.metricId)
+}
+
+</script>
