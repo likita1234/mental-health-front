05b067fc7413af46c647824dc2195c69d0c11e4e
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:24 +0000

    CustomQuestion component created to handle question detail type and display accordingly

---
+++ src/components/question/components/CustomQuestion.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, computed, inject } from 'vue';
+import { QuestionType } from '@/constants'
+
+// Props
+const props = defineProps({
+    modelValue: String,
+    question: Object
+});
+
+const appState = inject('appState')
+
+// Component states
+const answer = ref(null)
+
+// Computed properties
+const label = computed(() => {
+    return props.question?.title[appState.lang]
+})
+
+const description = computed(() => {
+    return props.question?.description[appState.lang]
+})
+
+const type = computed(() => {
+    return props.question.type
+})
+
+const options = computed(() => {
+    return props.question?.options
+})
+
+
+
+</script>
+
+<template>
+    <div class="field col">
+        <!-- Input text type -->
+        <template v-if="type === QuestionType.TEXT">
+            <BaseTextInput v-model="answer" :label="label" />
+        </template>
+
+        <template v-if="type === QuestionType.RADIO || type === QuestionType.CHECKBOX">
+            <BaseSelectionInput v-model="answer" :label="label" :options="options"
+                :radio="type === QuestionType.RADIO" />
+        </template>
+
+        <template v-if="type === QuestionType.RATINGS">
+            <BaseRating v-model="answer" :label="label" :description="description" :cancel="true" />
+        </template>
+
+    </div>
+
+</template>
