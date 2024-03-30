ef4dcf703224084113363fb352fdb96d79f3e13d
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:43 +0000

    Language selection added on survey form

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import LandingNavbar from './components/LandingNavbar.vue'
+
+const route = useRoute()
+
+const isSurveyRoute = computed(() => {
+    return route.name === 'survey-form'
+})
 </script>
 
 <template>
@@ -11,6 +19,9 @@ import LandingNavbar from './components/LandingNavbar.vue'
         <div class="flex flex-column justify-content-center gap-5 h-fit">
             <LandingNavbar />
             <div class="landing-container">
+                <div v-if="isSurveyRoute" class="flex justify-content-center">
+                    <language-selection />
+                </div>
                 <router-view></router-view>
             </div>
         </div>
