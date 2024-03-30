9275df3e2330b1abf754d0b2dad4b541a584ce43
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:27 +0000

    Survey Component created to show ongoing surveys

---
+++ src/pages/modules/Survey.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+<template>
+    <div class="flex justify-content-center">
+        <div class="w-6 surface-section px-4 py-8 md:px-6 lg:px-8 text-center">
+            <div class="mb-6 font-bold text-3xl">
+                <span class="text-blue-600">Our Ongoing Surveys </span>
+            </div>
+            <!-- <div class="text-700 mb-6">Please feel free to contribute to any of them.</div> -->
+            <div class="grid mt-4 justify-content-center">
+                <div class="col-12 md:col-4 mb-4 px-4">
+                    <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                        <font-awesome-icon :icon="['fas', 'brain']" size="3x" />
+
+                    </span>
+                    <div class="text-900 text-xl mb-3 font-bold text-primary">Mental Health Assessment</div>
+                    <span class="text-700 line-height-3">Understanding the Spectrum: Mental Health Assessment
+                        Resources.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
