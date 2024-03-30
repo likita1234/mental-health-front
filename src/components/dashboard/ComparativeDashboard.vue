34bec58737aa6905b99c0e5e3b969200a6d8a3cb
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:42 +0000

    Comparative Dashboard setup with metric data

--- src/components/dashboard/ComparativeDashboard.vue
+++ src/components/dashboard/ComparativeDashboard.vue
@@ -1,21 +1,26 @@
 <template>
     <div>
-        <dashboard-selection-dropdown :type="'comparative'" @dashboard-selected="loadSelectedDashboard" class=""/>
+        <dashboard-selection-dropdown :type="'comparative'" @dashboard-selected="loadSelectedDashboard" class="" />
         <!-- Charts  -->
-        <div class="flex">
-            <div>
-
-            </div>
+        <div v-if="metricIds && metricIds.length > 0" class="flex">
+            <metric-data v-for="metric in metricIds" :metricId="metric.metricId" :key="metric.metricId" class="col-12 md:col-6"/>
         </div>
     </div>
 </template>
 
 <script setup>
 import DashboardSelectionDropdown from '@/components/utils/DashboardSelectionDropdown.vue'
+import MetricData from '@/components/metric/MetricData.vue'
+import { ref } from 'vue';
 
+
+// Component states
+const metricIds = ref([])
 
 // Actions
-const loadSelectedDashboard = async (dashboardId) => {
-    console.log(dashboardId)
+const loadSelectedDashboard = async (dashboardDetails) => {
+    // console.log(dashboardDetails)
+    const { _id, title, description, metrics } = dashboardDetails
+    metricIds.value = metrics
 }
 </script>
