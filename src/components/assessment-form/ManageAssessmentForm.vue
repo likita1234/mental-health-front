6b1ba65c46306d2b76476d91006665a9187bd40c
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:22 +0000

    Landing page for assessment form added

---
+++ src/components/assessment-form/ManageAssessmentForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import AssessmentFormTable from './AssessmentFormTable.vue'
+import AssessmentForm from './AssessmentForm.vue'
+
+const formDialog = ref(false)
+
+const toggleFormDialog = (flag) => {
+    formDialog.value = flag
+}
+</script>
+
+
+<template>
+    <div>
+        <Toolbar class="mb-4">
+            <template #start>
+                <Button label="New" icon="pi pi-plus" severity="success" @click="toggleFormDialog(true)" class="mr-2" />
+            </template>
+        </Toolbar>
+        <div class="card">
+            <AssessmentFormTable />
+        </div>
+        <!-- Question Form Dialog -->
+        <AssessmentForm v-if="formDialog" @hide-dialog="toggleFormDialog(false)" />
+    </div>
+</template>
