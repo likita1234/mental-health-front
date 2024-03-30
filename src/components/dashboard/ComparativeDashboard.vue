fbe5f4895451e866e03a961a8432998f78396834
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:43 +0000

    DataRequests added to metricData and metric data loaded into comparative dashboards

--- src/components/dashboard/ComparativeDashboard.vue
+++ src/components/dashboard/ComparativeDashboard.vue
@@ -3,7 +3,9 @@
         <dashboard-selection-dropdown :type="'comparative'" @dashboard-selected="loadSelectedDashboard" class="" />
         <!-- Charts  -->
         <div v-if="metricIds && metricIds.length > 0" class="flex flex-wrap">
-            <metric-data v-for="metric in metricIds" :metricId="metric.metricId" :key="metric.metricId" class="col-12"/>
+            <!-- dataRequested props allows metricdata component to know that we are expecting a data -->
+            <metric-data v-for="metric in metricIds" :metricId="metric.metricId" :key="metric.metricId" dataRequested
+                class="col-12" @metric-data-loaded="loadMetrics" />
         </div>
     </div>
 </template>
@@ -13,14 +15,18 @@ import DashboardSelectionDropdown from '@/components/utils/DashboardSelectionDro
 import MetricData from '@/components/metric/MetricData.vue'
 import { ref } from 'vue';
 
-
 // Component states
 const metricIds = ref([])
+const comparativeMetrics = ref([])
 
 // Actions
 const loadSelectedDashboard = async (dashboardDetails) => {
     // console.log(dashboardDetails)
     const { _id, title, description, metrics } = dashboardDetails
     metricIds.value = metrics
+}
+
+const loadMetrics = (metricData) => {
+    comparativeMetrics.value.push(metricData)
 }
 </script>
