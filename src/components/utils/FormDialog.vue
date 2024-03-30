e06f2517eff986038e48d1f47b9a2e7e0b41dad6
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:15 +0000

    FormDialog component created

---
+++ src/components/utils/FormDialog.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['hide-dialog', 'submitted'])
+
+const props = defineProps({
+    header: String,
+    width: String
+})
+
+// component states
+const visible = ref(true)
+
+</script>
+
+<template>
+    <Dialog v-model:visible="visible" modal :header="header" :style="{ width }"
+        :breakpoints="{ '1199px': '60vw', '575px': '80vw' }" @hide="emit('hide-dialog')">
+        <!-- Our content goes here -->
+        <template #header>
+            <slot name="header"/>
+        </template>
+
+        <slot />
+
+
+        <template #footer>
+            <Button label="Cancel" class="mr-2" severity="danger" raised />
+            <Button label="Submit" raised />
+        </template>
+    </Dialog>
+</template>
+
