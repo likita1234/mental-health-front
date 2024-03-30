7c19939ba3f0c185ef0de6a95b8a64100c39db08
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:37 +0000

    MetricData component isolated from chart related operations, Chart operations set for BaseChart component

--- src/components/metric/MetricData.vue
+++ src/components/metric/MetricData.vue
@@ -1,20 +1,19 @@
 <template>
     <div class="card">
         <div id="card-header" class="order-tabs">
-            <h4 class="text-center">{{ title + ' (' + type + ')' }}</h4>
+            <h4 class="text-center">{{ title }}</h4>
         </div>
-        <div class="card-body">
-            <!-- <Chart type="pie" :data="chartData" :options="chartOptions" class="w-full md:w-30rem" /> -->
+        <div v-if="chartData" class="card-body">
+            <base-chart :type="type" :chartData="jsonData" />
         </div>
-
     </div>
 </template>
 
 <script setup>
+import BaseChart from '@/components/charts/BaseChart.vue'
 import { onMounted, ref, computed } from 'vue';
 
 import { metricStore } from '@/stores';
-import { generateRandomColor } from '@/constants/color'
 
 // Props
 const props = defineProps({
@@ -27,8 +26,6 @@ const props = defineProps({
 // Component states
 const metricDetails = ref(null)
 
-const chartData = ref(null)
-
 // Computed properties
 const title = computed(() => {
     return metricDetails.value?.title
@@ -38,37 +35,18 @@ const type = computed(() => {
     return metricDetails.value?.chartType
 })
 
-onMounted(async () => {
-    await loadMetricDetails()
+// Returns the json data inside the metricDetails
+const jsonData = computed(() => {
+    return metricDetails.value?.metricData?.data;
+})
 
-    loadChartDetails()
+onMounted(() => {
+    loadMetricData()
 })
 
 // Actions
-const loadMetricDetails = async () => {
+const loadMetricData = async () => {
     metricDetails.value = await metricStore.getMetricData(props.metricId)
-    console.log(metricDetails.value)
-}
-
-const loadChartDetails = async () => {
-    chartData.value = await setChartData();
-}
-
-const setChartData = async () => {
-    const metricData = metricDetails.value.metricData.data;
-    // background colors
-    let bgColors = []
-    for (let i = 0; i < metricData?.length; i++) {
-        bgColors.push(generateRandomColor())
-    }
-    return {
-        labels: metricData?.map(dataObj => dataObj?.label),
-        datasets: {
-            data: metricData?.map(dataObj => dataObj?.count),
-            backgroundColor: bgColors,
-            hoverBackgroundColor: bgColors,
-        }
-    }
 }
 
 </script>
