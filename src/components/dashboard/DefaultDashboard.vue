e7410ed40a0ba5d68c670be331d24f6531b64299
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:35 +0000

    DefaultDashboard component setup

---
+++ src/components/dashboard/DefaultDashboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ dashboard }}
+    </div>
+</template>
+
+<!-- At the moment there is no way to identify default dashboard so we need to preset the exact dashboard id -->
+<script setup>
+import { onMounted, ref } from 'vue';
+
+import { dashboardStore } from '@/stores'
+
+// Component states
+const dashboard = ref(null)
+
+onMounted(() => {
+    loadDashboardDetails()
+})
+
+// Actions
+const loadDashboardDetails = async () => {
+    // Set dashboard id hardcoded for now
+    const dashboardId = "65f77004da281f65e8a3952e";
+    dashboard.value = await dashboardStore.getDashboardDetails(dashboardId)
+}
+
+</script>
