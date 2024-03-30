b3cf6e536e82b250825a4d20498c10bb4d6adf3f
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:09 +0000

    Error State constants added

---
+++ src/contants/error-states.js
@@ -0,0 +1,9 @@
+export const ErrorStates = Object.freeze({
+  ERROR: 'Error',
+  NOT_FOUND: 'Not Found',
+  FAILED: 'Failed',
+  UNDEFINED: 'Undefined',
+  INVALID: 'Invalid',
+  UNAUTHENTICATED: 'Unauthenticated',
+  UNAUTHORIZED: 'Unauthorized'
+})
