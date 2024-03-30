f6599c1dce1ab6894fc70f41657d30ba4fab2d4c
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:38 +0000

    BaseChart component created to handle all the charts and visualzation checks and switches based on type of chart PieChart default for now

---
+++ src/components/charts/BaseChart.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <pie-chart :chartOptions="chartOptions" :chartData="chartData" />
+    </div>
+</template>
+
+<script setup>
+import PieChart from './PieChart.vue';
+import { ref, computed } from 'vue';
+import { generateRandomColors } from '@/constants/color'
+
+// Props
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'table'
+    },
+    jsonData: {
+        type: Object,
+    }
+})
+
+// Component states
+const chartOptions = ref({
+    responsive: true,
+    maintainAspectRatio: false,
+});
+
+// Computed properties
+const chartData = computed(() => {
+    const metricData = props.jsonData
+    // background colors
+    let bgColors = generateRandomColors(metricData?.length)
+    return {
+        labels: metricData?.map(dataObj => dataObj.label),
+        datasets: [{
+            backgroundColor: bgColors,
+            data: metricData?.map(dataObj => dataObj.count),
+        }]
+    }
+})
+</script>
