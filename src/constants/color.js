bd0a61b3dcd4b98e117527d3c615da04b4275826
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:36 +0000

    Colors contants configured

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,6 +1,8 @@
 export const Color = Object.freeze({
-    RED: '--custom-red',
-    AMBER: '--custom-amber',
-    GREEN: '--custom-green',
-    BLUE: '--custom-blue',
-});
+  Red: '#FF0000',
+  Blue: '#0000FF',
+  Green: '#008000',
+  Yellow: '#FFFF00',
+  Orange: '#FFA500',
+  Purple: '#800080'
+})
