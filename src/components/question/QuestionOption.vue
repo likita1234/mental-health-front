ddb7b4874250cf400d14bfe517a11bd12af36550
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:19 +0000

    Nepali and English input added on Question Options

--- src/components/question/QuestionOption.vue
+++ src/components/question/QuestionOption.vue
@@ -21,10 +21,13 @@ const { formErrors } = storeToRefs(formErrorStore)
 
 
 <template>
-    <div class="grid grid-cols-2 gap-2">
-        <!-- Option Name -->
-        <BaseTextInput v-model="option.optionLabel" label="Option Label"
-            :errorMessage="formErrors[`options[${props.keyIndex}]` + '.optionLabel']" />
+    <div class="grid grid-cols-3 gap-2">
+        <!-- Option Label English-->
+        <BaseTextInput v-model="option.title.english" label="Label"
+            :errorMessage="formErrors[`options[${props.keyIndex}]` + '.title.english']" />
+        <!-- Option Label Nepali -->
+        <BaseTextInput v-model="option.title.nepali" label="Label (Nepali)"
+            :errorMessage="formErrors[`options[${props.keyIndex}]` + '.title.nepali']" />
         <!-- Option Value -->
         <BaseNumberInput v-model="option.optionValue" label="Value"
             :errorMessage="formErrors[`options[${props.keyIndex}]` + '.optionValue']" />
