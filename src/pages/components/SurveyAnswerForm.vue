f6272e79077660d34194c91a9a540523d851d109
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:32 +0000

    SurveyForm component created instead of relying on CustomAssessment

--- src/pages/components/SurveyAnswerForm.vue
+++ src/pages/components/SurveyAnswerForm.vue
@@ -1,5 +1,8 @@
 <script setup>
-import CustomAssessment from '../../components/assessment-form/CustomAssessment.vue'
+import { onMounted, ref, computed, inject } from 'vue';
+
+import { formStore } from '@/stores';
+import { storeToRefs } from 'pinia';
 
 // props
 const props = defineProps({
@@ -8,12 +11,112 @@ const props = defineProps({
         required: true
     }
 })
+// Global state
+const appState = inject('appState')
+
+// Store states
+const { formDetails } = storeToRefs(formStore)
+
+// Component states
+const activeStep = ref(0) //starts with index 0
+
+// Computed properties
+const formTitle = computed(() => {
+    return formDetails.value?.title[appState.lang]
+})
+
+const formDescription = computed(() => {
+    return formDetails.value?.description[appState.lang]
+})
+
+// Returns a list of section items for Steps
+const sectionItems = computed(() => {
+    let items = []
+    for (let i = 0; i < formDetails.value?.sections?.length; i++) {
+        items.push({ label: 'Section ' + (i + 1) })
+    }
+    return items
+})
+
+// Returns the current active section based on activeStep index
+const activeSection = computed(() => {
+    return formDetails.value?.sections[activeStep.value];
+})
+
+const hasPreviousStep = computed(() => {
+    return activeStep.value !== 0
+})
+
+const hasNextStep = computed(() => {
+    return (activeStep.value + 1) < formDetails.value?.sections?.length
+})
+
+
+// Navigate the steps
+const toggleStep = (stepFlag) => {
+    stepFlag ? activeStep.value++ : activeStep.value--
+    // Scroll window
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+onMounted(() => {
+    setupAssessmentDetails()
+})
+
+// Actions
+
+const setupAssessmentDetails = async () => {
+    // First load assessment details
+    await loadAssessmentDetails()
+}
+const loadAssessmentDetails = async () => {
+    const formId = props.id
+    await formStore.fetchFormDetails(formId)
+}
+
 
 </script>
 <template>
     <div class="flex justify-content-center">
-        <div class="w-full lg:w-10">
-            <CustomAssessment :id="id" />
+        <div class="card p-5 h-full w-full md:w-8">
+            <h2 class="text-center">{{ formTitle }}</h2>
+            <!-- Form Description -->
+            <p class="text-center"> {{ formDescription }}</p>
+            <Divider />
+            <div v-if="sectionItems.length">
+                <Steps v-model:activeStep="activeStep" :model="sectionItems" class="custom-steps">
+                    <template #item="{ item, active }">
+                        <span class="border-primary border-1 p-1 cursor-pointer"
+                            :class="{ 'bg-primary': active, 'surface-overlay text-primary': !active }">
+                            {{ item.label }}
+                        </span>
+                    </template>
+                </Steps>
+            </div>
+            <Divider />
+
+            <div v-if="activeSection" class="section-container">
+                <div class="flex flex-column justify-content-center align-items-center">
+                    <!-- Section Title Here -->
+                    <h4>{{ activeSection.title[appState.lang] }}</h4>
+                    <!-- Section Description Here -->
+                    <p v-if="activeSection.description">{{ activeSection.description[appState.lang] }}</p>
+                    <Divider />
+                </div>
+                <div class="questions-container">
+                    <!-- Section Questions Here -->
+                    <CustomQuestion v-for="question in activeSection.questions" v-model="question.answer"
+                        :question="question" :key="question._id" />
+                </div>
+                <Divider />
+                <div class="flex mb-2 gap-2 justify-content-between">
+                    <Button @click="toggleStep(false)" label="Previous" :disabled="!hasPreviousStep" />
+                    <Button @click="toggleStep(true)" label="Next" :disabled="!hasNextStep" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
