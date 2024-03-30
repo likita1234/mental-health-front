aadf970a7e5569f5ee1873d6045477275a00e9e8
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:30 +0000

    Css changes on landing page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -8,29 +8,11 @@ import LandingNavbar from './components/LandingNavbar.vue'
             <img src="/images/logo/logo-transparent.png" alt="app-logo" class="app-logo w-full">
         </div>
         <!-- NavigationBar -->
-        <div class="flex flex-column justify-content-center gap-7 h-full">
+        <div class="flex flex-column justify-content-center gap-5 h-fit">
             <LandingNavbar />
-            <router-view></router-view>
+            <div class="landing-container">
+                <router-view></router-view>
+            </div>
         </div>
     </div>
 </template>
-
-
-<style lang="scss">
-/*==================================================================
-[ Main Container ]*/
-.limiter {
-    margin: 0 auto;
-    // background: -webkit-linear-gradient(90deg, #359CA0, #38B98C);
-    // background: -o-linear-gradient(90deg, #359CA0, #38B98C);
-    // background: -moz-linear-gradient(90deg, #359CA0, #38B98C);
-    // background: linear-gradient(90deg, #359CA0, #38B98C);
-
-    background-image: url("images/overlay.png"), url("images/bg.jpg");
-    background-repeat: repeat, no-repeat;
-    background-size: auto, 100% 100%;
-    height: 100vh;
-    background-attachment: fixed;
-    // overflow-y: scroll;
-}
-</style>
