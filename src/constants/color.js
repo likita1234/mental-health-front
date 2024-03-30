af5ea2cec9ef5348b393a0d08c7a5916ab2bd9a7
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:37 +0000

    Random colors generator added New colors added

--- src/constants/color.js
+++ src/constants/color.js
@@ -6,3 +6,20 @@ export const Color = Object.freeze({
   Orange: '#FFA500',
   Purple: '#800080'
 })
+
+export const generateRandomColors = (n = null) => {
+  const colorValues = Object.values(Color)
+  // If n has value then return array of colors
+  if (n) {
+    const randomColors = []
+    for (let i = 0; i < n; i++) {
+      const randomIndex = Math.floor(Math.random() * colorValues.length)
+      randomColors.push(colorValues[randomIndex])
+    }
+    return randomColors
+  } else {
+    // Else just return a random color
+    const randomIndex = Math.floor(Math.random() * colorValues.length)
+    return colorValues[randomIndex]
+  }
+}
