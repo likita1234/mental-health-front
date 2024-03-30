c67487180444cdabde0d11285f1155051c4b2a8b
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:34 +0000

    Chart type enums added and configured

---
+++ src/constants/chart-type.js
@@ -0,0 +1,7 @@
+export const ChartType = Object.freeze({
+  TABLE: 'table',
+  BAR: 'bar',
+  LINE: 'line',
+  PIE: 'pie',
+  DOUGHNUT: 'doughnut'
+})
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
 import { AssessmentFormType } from './assessment-form-type'
+import { ChartType } from './chart-type'
 import { ErrorStates } from '@/constants/error-states'
 import { LanguageType } from './language-type'
 import { QuestionType } from '@/constants/question-type'
 import { UserRole } from '@/constants/user-role'
 
-export { AssessmentFormType, ErrorStates, LanguageType, QuestionType, UserRole }
+export { AssessmentFormType, ChartTypes, ErrorStates, LanguageType, QuestionType, UserRole }
