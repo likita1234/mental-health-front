b3e8588c8b3e43993bbc1a77d0d5dc61dd717d84
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:30 +0000

    SurveyAnswerForm component created

---
+++ src/pages/components/SurveyAnswerForm.vue
@@ -0,0 +1,19 @@
+<script setup>
+import CustomAssessment from '../../components/assessment-form/CustomAssessment.vue'
+
+// props
+const props = defineProps({
+    id: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+<template>
+    <div class="flex justify-content-center">
+        <div class="w-full lg:w-10">
+            <CustomAssessment :id="id" />
+        </div>
+    </div>
+</template>
