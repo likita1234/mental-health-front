40d300c0ae8ec7ab1984610afd380e5beee3e92f
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:44 +0000

    Data analysis file created to handle data analysis related process on the front end Weighted mean and standard deviation calculation function added

---
+++ src/utils/data-analysis.js
@@ -0,0 +1,39 @@
+export const calculateWeightedMeanAndSD = (values, frequencies) => {
+  //  Function to calculate weighted
+  function calculateWeightedMean(values, frequencies) {
+    let sum = 0
+    let totalFrequency = 0
+    for (let i = 0; i < values.length; i++) {
+      sum += values[i] * frequencies[i]
+      totalFrequency += frequencies[i]
+    }
+    return sum / totalFrequency
+  }
+
+  // Function to calculate variance
+  function calculateVariance(values, frequencies, mean) {
+    let sum = 0
+    let totalFrequency = 0
+    for (let i = 0; i < values.length; i++) {
+      sum += frequencies[i] * Math.pow(values[i] - mean, 2)
+      totalFrequency += frequencies[i]
+    }
+    return sum / totalFrequency
+  }
+
+  // Function to calculate standard deviation
+  function calculateStandardDeviation(variance) {
+    return Math.sqrt(variance)
+  }
+  // Calculate Mean
+  const weightedMean = calculateWeightedMean(values, frequencies)
+
+  // Calculate Variance
+  const variance = calculateVariance(values, frequencies, weightedMean)
+
+  // Calculate Standard Deviation
+  const standardDeviation = calculateStandardDeviation(variance)
+
+  // Return an object containing both mean and standard deviation
+  return { mean, standardDeviation }
+}
