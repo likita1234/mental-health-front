09a667162425b481fc045d39bd9eff344e5b2e39
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 11:41:18 +0000

    Events configured on FormDialog

--- src/components/utils/FormDialog.vue
+++ src/components/utils/FormDialog.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 
-const emit = defineEmits(['hide-dialog', 'submitted'])
+const emit = defineEmits(['hide-dialog', 'submit'])
 
 const props = defineProps({
     header: String,
@@ -18,15 +18,17 @@ const visible = ref(true)
         :breakpoints="{ '1199px': '60vw', '575px': '80vw' }" @hide="emit('hide-dialog')">
         <!-- Our content goes here -->
         <template #header>
-            <slot name="header"/>
+            <slot name="header" />
         </template>
 
         <slot />
 
 
         <template #footer>
-            <Button label="Cancel" class="mr-2" severity="danger" raised />
-            <Button label="Submit" raised />
+            <div class="mt-2">
+                <Button label="Cancel" class="mr-2" severity="danger" raised />
+                <Button label="Submit" raised @click="emit('submit', true)" />
+            </div>
         </template>
     </Dialog>
 </template>
