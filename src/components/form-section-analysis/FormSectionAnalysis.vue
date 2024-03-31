88a6e9dbef241a8955a163a21aa45cacc106da43
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 13:36:13 +0100

    FormSectionAnalysis component created for visualization of form and section related tables

--- src/components/form-section-analysis/FormSectionAnalysis.vue
+++ src/components/form-section-analysis/FormSectionAnalysis.vue
@@ -1,5 +1,38 @@
+<script setup>
+import FormSectionSelectionDialog from '@/components/common/dialogs/form-section-selection.vue'
+import { ref, computed } from 'vue';
+
+// Component states
+const dialogVisible = ref(false)
+const analysisIds = ref(null)
+
+// Computed Properties
+const hasAnalysisIds = computed(() => {
+    return analysisIds.value?.formId !== null && analysisIds.value?.sectionId !== null
+})
+
+// Actions
+const analysisDataSelected = (event) => {
+    const { formId, sectionId } = event
+    analysisIds.value = { formId, sectionId }
+    toggleDialog(false)
+}
+
+const toggleDialog = (flag) => {
+    dialogVisible.value = flag
+}
+
+</script>
+
 <template>
     <div>
-        
+        <div class="flex justify-content-around">
+            <Button label="Choose Assessment Form and Section" @click="toggleDialog(true)" />
+        </div>
+        <!-- Dialog for form and section selection -->
+        <form-section-selection-dialog v-if="dialogVisible" @form-section-selected="analysisDataSelected"
+            @hide-dialog="toggleDialog(false)" />
+
+
     </div>
 </template>
