4714995c9c9fd2e325d0e37ddfe954fb19be2377
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 11:35:19 +0100

    Assessment form selection dropdown created that emits selected form details

---
+++ src/components/common/dropdowns/assessment-form-selection.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref, onMounted, watch, } from 'vue'
+import { storeToRefs } from 'pinia'
+
+import { formStore } from '@/stores'
+
+const emit = defineEmits(['form-selected'])
+
+// Props
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'normal'
+    }
+})
+
+// Store states
+const { allForms } = storeToRefs(formStore)
+
+// Component States
+const selectedForm = ref(null)
+
+// Watchers for analysis of form selection 
+watch(() => selectedForm.value, () => {
+    if (selectedForm.value) {
+        const formDetails = allForms.value.find(form => form._id === selectedForm.value)
+        console.log(formDetails)
+        emit('form-selected', formDetails)
+    }
+})
+
+onMounted(() => {
+    loadAssessmentForms()
+})
+
+// Actions
+// At the moment assessment forms are very limited so we are not utilizing any parameters
+const loadAssessmentForms = async () => {
+    // Check if allForms already have value, if yes then no need to make another request
+    if (allForms.value && allForms.value?.length <= 0) {
+        await formStore.fetchAllForms()
+    }
+}
+</script>
+
+<template>
+    <div>
+        <BaseDropdown v-model="selectedForm" :label="'Start by selecting an assessment form'" :options="allForms"
+            optionLabel="engTitle" optionValue="_id" placeholder="Select a form" :showClear="false" />
+    </div>
+</template>
