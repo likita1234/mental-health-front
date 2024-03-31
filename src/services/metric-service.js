d383b55ca810a880e28a50e95a1c0ffb7f77ab4f
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 13:50:20 +0100

    Services and stores created to fetch metric data  for table analysis by form id and section id

--- src/services/metric-service.js
+++ src/services/metric-service.js
@@ -12,6 +12,14 @@ class MetricService {
       return new AppError(error.statusCode, 'Failed to fetch metric data')
     }
   }
+  // Fetch table analysis data by form id and section id
+  static async getTableAnalysisByFormIdSectionId(formId, sectionId) {
+    try {
+      return await authApi.get(`${METRIC_API}/${formId}/${sectionId}/data`)
+    } catch (error) {
+      return new AppError(error.statusCode, 'Failed to fetch table analysis data')
+    }
+  }
 }
 
 export default MetricService
