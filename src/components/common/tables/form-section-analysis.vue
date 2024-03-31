e0a455f464d8c260be878a828b903d6ebc07e889
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 13:42:10 +0100

    Form Section Analysis table WIP

---
+++ src/components/common/tables/form-section-analysis.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+const props = defineProps({
+    analysisIds: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div>
+        Yo
+        {{ analysisIds }}
+    </div>
+</template>
