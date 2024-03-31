224d8d04d196f08bac82b8f8b88fcf2e51723a18
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 15:34:56 +0100

    Form Section Analysis table created to visualize tabular representation of Form's Section data

--- src/components/common/tables/form-section-analysis.vue
+++ src/components/common/tables/form-section-analysis.vue
@@ -1,16 +1,43 @@
 <script setup>
+// import { ref, onMounted } from 'vue';
 
 const props = defineProps({
-    analysisIds: {
-        type: Object,
+    tableHeader: {
+        type: String,
+        default: 'Section Title'
+    },
+    tableData: {
+        type: Array,
         required: true
     }
 })
+// Actions
 </script>
 
 <template>
-    <div>
-        Yo
-        {{ analysisIds }}
+    <div class="card">
+        <DataTable :value="tableData" rowGroupMode="rowspan" groupRowsBy="questionTitle" sortMode="single"
+            sortField="questionTitle" :sortOrder="1">
+            <template #empty> No data found. </template>
+            <template #header>
+                <div class="flex">
+                    <span class="text-xl text-900 font-bold">{{ tableHeader }}</span>
+                </div>
+            </template>
+            <Column field="questionTitle" header="Questions" style="min-width: 200px">
+                <template #body="slotProps">
+                    <div class="flex align-items-center gap-2">
+                        <span>{{ slotProps.data.questionTitle }}</span>
+                    </div>
+                </template>
+            </Column>
+            <Column field="optionTitle" header="Options" style="min-width: 200px" />
+            <Column field="value" header="Frequency" style="min-width: 100px" />
+            <Column field="percent" header="% Value" style="min-width: 150px">
+                <template #body="slotProps">
+                    <span>{{ slotProps.data.percent?.toFixed(2) }}</span>
+                </template>
+            </Column>
+        </DataTable>
     </div>
 </template>
