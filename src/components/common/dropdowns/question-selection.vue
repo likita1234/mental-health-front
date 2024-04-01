4ff6cc4e62a5625ddf82b1570c1009dc76e7390d
Likita Maharjan <likitaof2015@gmail.com>
2024-04-01 19:02:24 +0100

    Question selection dropdown component created

---
+++ src/components/common/dropdowns/question-selection.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { storeToRefs } from 'pinia'
+import { questionStore } from '@/stores'
+
+const emit = defineEmits(['question-selected'])
+
+const props = defineProps({
+    params: {
+        type: Object,
+        default: {}
+    }
+})
+
+// Store states
+const { allQuestions, params } = storeToRefs(questionStore)
+
+// Component states
+const selectedQuestion = ref(null)
+
+onMounted(() => {
+    loadQuestions()
+})
+
+// Actions
+const loadQuestions = async () => {
+    // Only load open end questions
+    params.value = props.params
+    await questionStore.fetchAllQuestions()
+}
+</script>
+
+<template>
+    <div>
+        <BaseDropdown v-model="selectedQuestion" :options="allQuestions" :placeholder="'Select a question'"
+            :optionLabel="'title.' + [appState.lang]" :optionValue="'_id'" filter
+            @change="emit('question-selected', $event)" />
+    </div>
+</template>
