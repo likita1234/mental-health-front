2f0486dbf8edd3bce288ede2c94eb3907d48b294
Likita Maharjan <likitaof2015@gmail.com>
2024-04-01 20:18:39 +0100

    Words passed as props Realignments of component states

--- src/components/plugins/custom-word-cloud.vue
+++ src/components/plugins/custom-word-cloud.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { ref } from 'vue';
 
-const words = ref([['accustomed', 6], ['adapt', 6], ['attended', 8], ['celebrations', 8], ['challenging', 6], ['communication',
-    6], ['completely', 6], ['concept', 6], ['country', 18], ['cuisine', 8], ['cultural', 30], ['culture', 10], ['customs',
-    8], ['differences', 8], ['different', 28], ['dynamics', 6], ['educational', 6], ['emphasis', 6], ['encountered', 10],
-['etiquette', 6], ['experienced', 18], ['festivals', 10], ['habits', 6], ['home', 10], ['interactions', 6], ['nepal',
-    24], ['overwhelming', 6], ['people', 10], ['personal', 12], ['punctuality', 8], ['quite', 6], ['realized', 8],
-['rituals', 6], ['shock', 26], ['social', 10], ['space', 10], ['surprised', 6], ['system', 12], ['time', 16],
-['tradition', 10], ['unfamiliar', 6]]);
-
+// Props
+const props = defineProps({
+    words: {
+        type: Array,
+        required: true
+    }
+})
+// Component states
+const animationDuration = 10000;
+const animationEasing = 'ease-in';
+const animationOverlap = .2;
+const enterAnimation = 'animated bounceIn';
+const leaveAnimation = 'animated hinge';
 const colors = ref(([, weight]) => {
     if (weight > 30) return 'DeepPink';
     if (weight > 25) return 'RoyalBlue';
@@ -19,11 +24,6 @@ const colors = ref(([, weight]) => {
     if (weight > 0) return 'FireBrick';
     return 'SlateGray';
 })
-const animationDuration = 10000;
-const animationEasing = 'ease-in';
-const animationOverlap = .2;
-const enterAnimation = 'animated bounceIn';
-const leaveAnimation = 'animated hinge';
 const rotationUnit = ref(([, weight]) => {
     if (weight > 30) return 'turn';
     if (weight > 25) return 'deg';
@@ -32,7 +32,6 @@ const rotationUnit = ref(([, weight]) => {
     if (weight > 10) return 'turn';
     return 'deg'
 })
-
 const rotation = ref(([, weight]) => {
     if (weight > 25) return 1;
     if (weight > 20) return 1 / 12;
@@ -45,8 +44,9 @@ const rotation = ref(([, weight]) => {
 <template>
     <div class="p-card">
         <!-- :rotation-unit="rotationUnit" -->
-        <vue-word-cloud style="height: 500px;" :words="words" :color="colors" :animationDuration="animationDuration"
-            :animation-easing="animationEasing" :animation-overlap="animationOverlap" :enter-animation="enterAnimation"
-            :leave-animation="leaveAnimation" :rotation-unit="rotationUnit" :rotation="rotation" />
+        <vue-word-cloud style="height: 500px;" :words="props.words" :color="colors"
+            :animationDuration="animationDuration" :animation-easing="animationEasing"
+            :animation-overlap="animationOverlap" :enter-animation="enterAnimation" :leave-animation="leaveAnimation"
+            :rotation-unit="rotationUnit" :rotation="rotation" :spacing=".5" />
     </div>
 </template>
