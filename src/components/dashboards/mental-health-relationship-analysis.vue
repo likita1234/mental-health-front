cb2fe1c28df4371aefb96aaa5192189ac5b12c50
Likita Maharjan <likitaof2015@gmail.com>
2024-04-03 20:47:38 +0100

    Relationship Dashboard data loaded on the component

--- src/components/dashboards/mental-health-relationship-analysis.vue
+++ src/components/dashboards/mental-health-relationship-analysis.vue
@@ -1,6 +1,6 @@
 <!-- By Default we will search for a relationship dashboard and load the first one if it exists -->
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { storeToRefs } from 'pinia'
 
 import { dashboardStore } from '@/stores'
@@ -9,23 +9,36 @@ import { DashboardType } from '@/constants'
 // Store states
 const { dashboards, params } = storeToRefs(dashboardStore)
 
+// Component states
+const dashboardData = ref(null)
+
 onMounted(() => {
-    loadRelationshipDashboard()
+    loadRelationshipDashboardId()
 })
 
-const loadRelationshipDashboard = async () => {
+// Load dashboards and fetch the id of the first relationship dashboard
+const loadRelationshipDashboardId = async () => {
+    // Set params for type
     params.value = { type: DashboardType.RELATIONSHIP }
-    // Load all dashboards with type relationship
-    const existingDashboard = await dashboardStore.fetchDashboards()
-    if (existingDashboard) {
-        console.log(existingDashboard)
+    // Load dashboards with type relationship
+    await dashboardStore.fetchDashboards()
+    // Fetch the dashboardId of first one and fetch its dashboard data
+    const dashboardId = dashboards.value[0]?._id
+    // If it exists then load dashboard data
+    if (dashboardId) {
+        await loadDashboardData(dashboardId)
     }
+}
+
+// load dashboard data
+const loadDashboardData = async (dashboardId) => {
+    dashboardData.value = await dashboardStore.getDashboardData(dashboardId)
 }
 
 </script>
 
 <template>
     <div>
-        
+        {{ dashboardData }}
     </div>
 </template>
