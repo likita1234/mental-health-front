b91cda35fb8b874cb6d685210571557a057c4960
Likita Maharjan <likitaof2015@gmail.com>
2024-04-04 22:30:09 +0100

    Url issue config fixes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'url';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
