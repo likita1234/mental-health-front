a986e27629a37b49228c1c42929b8728023e9601
Likita Maharjan <likitaof2015@gmail.com>
2024-04-07 16:22:17 +0100

    Access and NotFound pages  added

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="exception-body notfound-page">
+        <div class="exception-top">
+            <img src="/images/icons/icon-error.png" alt="not-found-image" />
+        </div>
+
+        <div class="exception-bottom">
+            <div class="exception-wrapper">
+                <span class="exception-summary">Page Not Found</span>
+                <span class="exception-detail">The resource you are looking for does not exist.</span>
+                <router-link :to="{ name: 'default-dashboard' }">
+                    <Button label="Go to Dashboard" icon="pi pi-home"></Button>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
