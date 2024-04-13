e090ac738cf7ec988b93cf136baa886c4be45b98
Likita Maharjan <likitaof2015@gmail.com>
2024-04-13 13:54:44 +0100

    Self assessment dashboard routes and menu configured

---
+++ src/components/dashboards/self-assessment.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { dashboardStore } from '@/stores';
+
+// const actualTitles = ref([])
+const overallData = ref(null)
+const testData = ref(null)
+
+onMounted(() => {
+    loadDashboardData()
+})
+
+const loadDashboardData = async () => {
+    const userId = '20240408_195203_238'
+    const { allTitles, data, total } = await dashboardStore.getPersonalSubmissionAnalysisData(userId)
+    overallData.value = data
+    testData.value = overallData.value[0]
+    console.log(allTitles)
+    console.log(overallData.value)
+}
+</script>
+
+
+<template>
+    <div>
+        <!-- {{ allTitles }} -->
+        {{ testData }}
+    </div>
+</template>
