8ca94813452d2654ceba53973bf9caff86854faa
Likita Maharjan <likitaof2015@gmail.com>
2024-04-13 15:11:56 +0100

    Chart label date formatter function added

--- src/utils/date-formatter.js
+++ src/utils/date-formatter.js
@@ -7,3 +7,14 @@ export const formatDisplayDate = (dateVal) => {
 export const formatDisplayDateToRequestDate = (dateVal) => {
   return dateVal ? DateTime.fromFormat(dateVal, 'dd-MM-yyyy').toFormat('yyyy-MM-dd') : null
 }
+
+export const formatLineChartDateLabels = (dateVal) => {
+  if (!dateVal) {
+    return 'Invalid or null input'
+  }
+  const date = DateTime.fromISO(dateVal, { zone: 'utc' })
+  if (!date.isValid) {
+    return 'Invalid date'
+  }
+  return date.toFormat('LLL-dd, yyyy')
+}
