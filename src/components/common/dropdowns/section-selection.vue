6ec1887191e0d73d810e24f5034ee86ff5c024a0
Likita Maharjan <likitaof2015@gmail.com>
2024-04-14 19:17:10 +0100

    modelvalue mode added on section selection along with filters as per form selected

--- src/components/common/dropdowns/section-selection.vue
+++ src/components/common/dropdowns/section-selection.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { ref, onMounted, } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 
 import { sectionStore } from '@/stores'
 
-const emit = defineEmits(['section-selected'])
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+    },
+    sectionFilters: {
+        type: Array,
+        default: []
+    }
+})
 
 // Store states
 const { allSections } = storeToRefs(sectionStore)
@@ -12,7 +22,17 @@ const { allSections } = storeToRefs(sectionStore)
 // Component States
 const selectedSection = ref(null)
 
+// Computed properties
+const filteredSections = computed(() => {
+    return props.sectionFilters?.length > 0 ? allSections.value.filter(section => props.sectionFilters.includes(section._id)) : allSections.value
+})
+
+watch(() => props.modelValue, () => {
+    selectedSection.value = props.modelValue
+})
+
 onMounted(() => {
+    selectedSection.value = props.modelValue
     loadSections()
 })
 
@@ -21,10 +41,11 @@ onMounted(() => {
 const loadSections = async () => {
     await sectionStore.fetchAllSections(true)
 }
+
 </script>
 
 <template>
-    <BaseDropdown v-model="selectedSection" label="Select a section" :options="allSections"
+    <BaseDropdown v-model="selectedSection" label="Select a section" :options="filteredSections"
         :optionLabel="'title.' + [appState.lang]" optionValue="_id" placeholder="Choose an option"
-        @change="emit('section-selected', $event)" />
+        @change="emit('update:modelValue', $event)" />
 </template>
