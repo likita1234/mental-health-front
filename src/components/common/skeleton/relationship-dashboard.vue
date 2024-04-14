29f35444f09057ffef2d18b984738c1a00d92cfa
Likita Maharjan <likitaof2015@gmail.com>
2024-04-15 00:02:50 +0100

    Datatable loader format added for relationship dashboard

--- src/components/common/skeleton/relationship-dashboard.vue
+++ src/components/common/skeleton/relationship-dashboard.vue
@@ -12,10 +12,28 @@
             <ProgressSpinner />
         </div>
         <div class="my-2 border-round surface-card">
-            <Skeleton width="100%" height="150px"></Skeleton>
+            <DataTable :value="data">
+                <Column field="TITLE" header="TITLE">
+                    <template #body>
+                        <Skeleton></Skeleton>
+                    </template>
+                </Column>
+                <Column field="TVALUE" header="T VALUE">
+                    <template #body>
+                        <Skeleton></Skeleton>
+                    </template>
+                </Column>
+                <Column field="PVALUE" header="P VALUE">
+                    <template #body>
+                        <Skeleton></Skeleton>
+                    </template>
+                </Column>
+            </DataTable>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+const data = ref(new Array(5));
 </script>
