95edc246b2a8a55804a57f96c9e775beb204d0f3
Likita Maharjan <likitaof2015@gmail.com>
2024-04-14 23:33:47 +0100

    Skeleton loader added for relationship dashboard

---
+++ src/components/common/skeleton/relationship-dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <div class="flex justify-content-between my-3 gap-3">
+            <Skeleton width="100%" height="5rem"></Skeleton>
+            <Skeleton width="100%" height="5rem"></Skeleton>
+            <Skeleton width="100%" height="5rem"></Skeleton>
+        </div>
+        <div class="my-2 border-round surface-card">
+            <Skeleton width="100%" height="150px"></Skeleton>
+        </div>
+        <div class="flex justify-content-center">
+            <ProgressSpinner />
+        </div>
+        <div class="my-2 border-round surface-card">
+            <Skeleton width="100%" height="150px"></Skeleton>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
