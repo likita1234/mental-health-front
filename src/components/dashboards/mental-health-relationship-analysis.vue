84ea61eda062cfb8c06e636f6b08ac686ea72b1d
Likita Maharjan <likitaof2015@gmail.com>
2024-04-14 23:34:08 +0100

    Loading effect added for relationship dashboard while loading data

--- src/components/dashboards/mental-health-relationship-analysis.vue
+++ src/components/dashboards/mental-health-relationship-analysis.vue
@@ -1,5 +1,6 @@
 <!-- By Default we will search for a relationship dashboard and load the first one if it exists -->
 <script setup>
+import RelationshipSkeleton from '@/components/common/skeleton/relationship-dashboard.vue'
 import PlotyHeatmap from '@/components/plugins/ploty-heatmap.vue'
 
 import { ref, onMounted, computed, watch } from 'vue';
@@ -9,6 +10,7 @@ import { dashboardStore } from '@/stores'
 import { convertRatingsDataToObject } from '@/utils/chart-helpers'
 import { correlationMatrix, generateHypothesisAnalysis } from '@/utils/data-analysis'
 import { DashboardType } from '@/constants'
+import AppResponse from '@/utils/app-response';
 
 // Store states
 const { dashboards, params } = storeToRefs(dashboardStore)
@@ -18,6 +20,7 @@ const dashboardData = ref(null)
 const blockContents = ref([])
 const datasets = ref([])
 const hypothesisData = ref([])
+const isLoading = ref(false)
 
 // Computed properties
 const convertedDashboardData = computed(() => {
@@ -36,22 +39,28 @@ watch(() => convertedDashboardData.value, () => {
     }
 })
 
-
 onMounted(() => {
     loadRelationshipDashboardId()
 })
 
 // Load dashboards and fetch the id of the first relationship dashboard
 const loadRelationshipDashboardId = async () => {
-    // Set params for type
-    params.value = { type: DashboardType.RELATIONSHIP }
-    // Load dashboards with type relationship
-    await dashboardStore.fetchDashboards()
-    // Fetch the dashboardId of first one and fetch its dashboard data
-    const dashboardId = dashboards.value[0]?._id
-    // If it exists then load dashboard data
-    if (dashboardId) {
-        await loadDashboardData(dashboardId)
+    try {
+        isLoading.value = true
+        // Set params for type
+        params.value = { type: DashboardType.RELATIONSHIP }
+        // Load dashboards with type relationship
+        await dashboardStore.fetchDashboards()
+        // Fetch the dashboardId of first one and fetch its dashboard data
+        const dashboardId = dashboards.value[0]?._id
+        // If it exists then load dashboard data
+        if (dashboardId) {
+            await loadDashboardData(dashboardId)
+        }
+    } catch (error) {
+        new AppResponse(400, error?.message ?? 'Something went wrong')
+    } finally {
+        isLoading.value = false
     }
 }
 
@@ -108,40 +117,43 @@ const generateCorrelation = async () => {
 </script>
 
 <template>
-    <div>
-        <div class="grid">
-            <div v-for="block in blockContents" :key="block" class="col-12 md:col-4">
-                <div class="p-card p-3 border-round">
-                    <div class="flex justify-content-between mb-3">
-                        <div>
-                            <span class="block text-500 font-medium mb-3">{{ block.label }}</span>
-                            <div class="text-900 font-medium text-xl">{{ block.total }}</div>
-                        </div>
-                        <div class="flex align-items-center justify-content-center border-round" :class="block.bgColor"
-                            style="width:2.5rem;height:2.5rem">
-                            <i class="pi text-xl" :class="[block.icon, block.color]"></i>
+    <relationship-skeleton v-if="isLoading" />
+    <div v-else>
+        <div>
+            <div class="grid">
+                <div v-for="block in blockContents" :key="block" class="col-12 md:col-4">
+                    <div class="p-card p-3 border-round">
+                        <div class="flex justify-content-between mb-3">
+                            <div>
+                                <span class="block text-500 font-medium mb-3">{{ block.label }}</span>
+                                <div class="text-900 font-medium text-xl">{{ block.total }}</div>
+                            </div>
+                            <div class="flex align-items-center justify-content-center border-round"
+                                :class="block.bgColor" style="width:2.5rem;height:2.5rem">
+                                <i class="pi text-xl" :class="[block.icon, block.color]"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div v-if="datasets && datasets.length > 0" class="card p-5">
-        <ploty-heatmap :datasets="datasets" />
-    </div>
-    <div v-else class="h-20rem flex justify-content-center align-items-center">
-        <h4 class="text-center"> Data not available for analysis</h4>
-    </div>
-    <div v-if="hypothesisData && hypothesisData.length > 0" class="card">
-        <DataTable ref="hypothesisDataDt" :value="hypothesisData">
-            <template #header>
-                <div class="flex flex-wrap gap-2 align-items-center justify-content-between">
-                    <h4 class="m-0">P Value Analysis based on Mental Health Score</h4>
-                </div>
-            </template>
-            <Column field="title" header="TITLE" style="width:10%" />
-            <Column field="tValue" header="T VALUE" style="width:10%" />
-            <Column field="pValue" header="P VALUE" style="width:10%" />
-        </DataTable>
+        <div v-if="datasets && datasets.length > 0" class="card p-5">
+            <ploty-heatmap :datasets="datasets" />
+        </div>
+        <div v-else class="h-20rem flex justify-content-center align-items-center">
+            <h4 class="text-center"> Data not available for analysis</h4>
+        </div>
+        <div v-if="hypothesisData && hypothesisData.length > 0" class="card">
+            <DataTable ref="hypothesisDataDt" :value="hypothesisData">
+                <template #header>
+                    <div class="flex flex-wrap gap-2 align-items-center justify-content-between">
+                        <h4 class="m-0">P Value Analysis based on Mental Health Score</h4>
+                    </div>
+                </template>
+                <Column field="title" header="TITLE" style="width:10%" />
+                <Column field="tValue" header="T VALUE" style="width:10%" />
+                <Column field="pValue" header="P VALUE" style="width:10%" />
+            </DataTable>
+        </div>
     </div>
 </template>
