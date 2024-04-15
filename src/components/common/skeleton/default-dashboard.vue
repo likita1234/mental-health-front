af20542ddda22acad1f2d313718c87506048189c
Likita Maharjan <likitaof2015@gmail.com>
2024-04-15 23:18:29 +0100

    Default Dashboard skeleton loader component created

---
+++ src/components/common/skeleton/default-dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <div class="flex justify-content-between my-3 gap-3">
+            <Skeleton width="100%" height="200px"></Skeleton>
+            <Skeleton width="100%" height="200px"></Skeleton>
+        </div>
+        <div class="flex justify-content-between my-3 gap-3">
+            <Skeleton width="100%" height="200px"></Skeleton>
+            <div class="flex justify-content-center">
+                <ProgressSpinner class="absolute z-2" />
+            </div>
+            <Skeleton width="100%" height="200px"></Skeleton>
+        </div>
+        <div class="flex justify-content-between my-3 gap-3">
+            <Skeleton width="100%" height="200px"></Skeleton>
+            <Skeleton width="100%" height="200px"></Skeleton>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
